fb63db49694ae816cb59018f35db2087d2f75ee7
Vincent Chen <vince78718@gmail.com>
2015-04-19 16:56:41 +0800

    Finish exercise 5 - study drills - convert the inches and pounds to centimeters and kilograms

--- exercise-05-more-variables-and-printing/ex5.rb
+++ exercise-05-more-variables-and-printing/ex5.rb
@@ -1,14 +1,18 @@
 name = 'Zed A. Shaw'
 age = 35 # not lie in 2009
 height = 74 # inches
+height_centermiter = height * 2.54
 weight = 180 # lbs
+weight_kilogram = weight * 0.45
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
 puts "Let's talk about #{name}."
 puts "He's #{height} inches tall"
+puts "He's #{height_centermiter} centermiters tall."
 puts "He's #{weight} pounds heavy."
+puts "He's #{weight_kilogram} kilograms heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair"
 puts "His teeth are usually #{teeth} depending on the coffe."
